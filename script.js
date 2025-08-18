2ae85dfd1ed9efba47a26256e04701e9a1f56f42
AdewemimoAdefunmbi <adefunmbiadewemimo@gmail.com>
2025-08-18 13:49:22 -0700

    I made changes on my index page, about page, faq page, contact page and css file

--- script.js
+++ script.js
@@ -54,3 +54,11 @@ function removeItem(name) {
     updateCart();
 }
 updateCart();
+
+
+// nav bar responsiveness
+const hamMenu= document.querySelector('.ham-menu')
+const offScreenMenu= document.querySelector('.off-screen-menu')
+hamMenu.addEventListener('click', () => {hamMenu.classList.toggle('active');
+offScreenMenu.classList.toggle('active')
+}) 
